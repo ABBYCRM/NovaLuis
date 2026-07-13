344e57d87a094e16074dab9ca2de69ef06bc226a
ABBYCRM <intake@abbycrm.com>
2026-07-13 13:52:07 -0400

    security: expand structured log redaction

--- artifacts/api-server/src/lib/logger.ts
+++ artifacts/api-server/src/lib/logger.ts
@@ -4,11 +4,33 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const logger = pino({
   level: process.env.LOG_LEVEL ?? "info",
-  redact: [
-    "req.headers.authorization",
-    "req.headers.cookie",
-    "res.headers['set-cookie']",
-  ],
+  redact: {
+    censor: "[REDACTED]",
+    paths: [
+      "req.headers.authorization",
+      "req.headers.cookie",
+      "req.headers['x-api-key']",
+      "req.headers['x-subscription-token']",
+      "req.headers['steel-api-key']",
+      "res.headers['set-cookie']",
+      "authorization",
+      "cookie",
+      "apiKey",
+      "accessToken",
+      "refreshToken",
+      "clientSecret",
+      "password",
+      "pin",
+      "token",
+      "fields.client_secret",
+      "fields.refresh_token",
+      "fields.access_token",
+      "err.config.headers.Authorization",
+      "err.config.headers.authorization",
+      "err.request.headers.Authorization",
+      "err.request.headers.authorization",
+    ],
+  },
   ...(isProduction
     ? {}
     : {
